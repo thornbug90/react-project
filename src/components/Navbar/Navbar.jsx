eb7b871d8284f891c06985bf518568a5a2dcee57
thornbug90 <crazydev0409@gmail.com>
2021-11-16 16:06:39 +0100

    19. Routes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,19 +7,19 @@ const Navbar = () => {
   return (
     <nav className={css.nav}>
       <div className={css.item}>
-        <a>Profile</a>
+        <a href="/profile">Profile</a>
       </div>
       <div className={css.item}>
-        <a>Messages</a>
+        <a href="/dialogs">Messages</a>
       </div>
       <div className={css.item}>
-        <a>News</a>
+        <a href="/news">News</a>
       </div>
       <div className={css.item}>
-        <a>Music</a>
+        <a href="/music">Music</a>
       </div>
       <div className={css.item}>
-        <a>Settings</a>
+        <a href="/settings">Settings</a>
       </div>
     </nav>
   );
