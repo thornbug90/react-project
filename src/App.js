62318233b49b298c20a4b8d1757186b747110cee
thornbug90 <crazydev0409@gmail.com>
2021-11-16 15:23:58 +0100

    18.Dialogs

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import './App.css';
 import Header from '../src/components/Header/Header';
 import Navbar from '../src/components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
+import Dialogs from './components/Dialogs/Dialogs';
 
 const App = () => {
   return (
     <div className="app-wrapper">
       <Header />
       <Navbar />
-      <Profile />
+      {/* <Profile /> */}
+      <div className="app-wrapper-content">
+        <Dialogs />
+      </div>
     </div>
   );
 };
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,15 +4,17 @@ import Post from './Post/Post';
 
 const MyPosts = () => {
   return (
-    <div className="posts">
+    <div>
       My posts
       <div>
         <textarea></textarea>
         <button>Add post</button>
         <button>Remove post</button>
       </div>
-      <Post message="Hi, how are you?" likes="15" />
-      <Post message="My first post" likes="20" />
+      <div className={css.post}>
+        <Post message="Hi, how are you?" likes="15" />
+        <Post message="My first post" likes="20" />
+      </div>
     </div>
   );
 };
