80f3a2262ce161a2671a61200b78581bcc0e4c3f
thornbug90 <crazydev0409@gmail.com>
2021-11-17 11:12:28 +0100

    24. UI BLL

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,11 @@ import css from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = () => {
+  let postsData = [
+    { id: 1, message: "Hi, how are you?", likes: 15 },
+    { id: 2, message: "My first post", likes: 20 },
+  ];
+
   return (
     <div className={css.postsBlock}>
       <h3>My posts</h3>
@@ -18,8 +23,8 @@ const MyPosts = () => {
         </div>
       </div>
       <div className={css.posts}>
-        <Post message="Hi, how are you?" likes="15" />
-        <Post message="My first post" likes="20" />
+        <Post message={postsData[0].message} likes={postsData[0].likes} />
+        <Post message={postsData[1].message} likes={postsData[1].likes} />
       </div>
     </div>
   );
