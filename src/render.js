3bc30cb673d20d99a36d37b0bc5aa68d83b4f451
thornbug90 <crazydev0409@gmail.com>
2021-11-19 14:39:02 +0100

    33. addPost, FLUX-conception

---
+++ src/render.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+export const rerender = (state, addPost) => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App state={state} addPost={addPost} />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+};
+
+reportWebVitals();
