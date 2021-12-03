3db0020d40000abb80b180b989c7e254fe9a0a91
thornbug90 <crazydev0409@gmail.com>
2021-12-03 13:35:58 +0100

    55. Pagination

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -7,15 +7,45 @@ class Users extends React.Component {
   // все side effects происходят здесь
   componentDidMount() {
     axios
-      .get('https://social-network.samuraijs.com/api/1.0/users')
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`
+      )
       .then(({ data }) => {
         this.props.setUsers(data.items);
+        this.props.setTotalUsersCount(data.totalCount);
       });
   }
 
+  onPageChanged = (numberPage) => {
+    this.props.setCurrentPage(numberPage);
+    axios
+      .get(`https://social-network.samuraijs.com/api/1.0/users?page=${numberPage}&count=${this.props.pageSize}`)
+      .then(({ data }) => {
+        this.props.setUsers(data.items);
+      });
+  };
+
   render() {
+    const pagesCount = Math.ceil(
+      this.props.totalUsersCount / this.props.pageSize
+    );
+    const pages = [];
+    for (let i = 1; i <= pagesCount; i += 1) {
+      pages.push(i);
+    }
+
     return (
       <div>
+        <div>
+          {pages.map((p) => (
+            <span
+              className={this.props.currentPage === p && css.selectedPage}
+              onClick={() => this.onPageChanged(p)}
+            >
+              {p}
+            </span>
+          ))}
+        </div>
         {this.props.users.map((u) => (
           <div key={u.id}>
             <span>
@@ -26,14 +56,15 @@ class Users extends React.Component {
                 />
               </div>
               <div>
-                {u.followed 
-                ? (<button onClick={() => this.props.unfollow(u.id)}>
+                {u.followed ? (
+                  <button onClick={() => this.props.unfollow(u.id)}>
                     Unfollow
-                  </button>)
-                : (<button onClick={() => this.props.follow(u.id)}>
+                  </button>
+                ) : (
+                  <button onClick={() => this.props.follow(u.id)}>
                     Follow
-                  </button>)
-                  }
+                  </button>
+                )}
               </div>
             </span>
             <span>
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -3,26 +3,33 @@ import {
   followAction,
   unfollowAction,
   setUsers,
+  setCurrentPage,
+  setTotalUsersCount
 } from './../../redux/usersReducer';
 
 // import Users from './Users';
-import Users from './Users'
+import Users from './Users';
 
-const mapStateToProps= (state) => {
+const mapStateToProps = (state) => {
   return {
     users: state.usersPage.users,
+    pageSize: state.usersPage.pageSize,
+    totalUsersCount: state.usersPage.totalUsersCount,
+    currentPage: state.usersPage.currentPage,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
     // диспатчим всегда результат работы action
-    follow: (userId) => dispatch(followAction(userId)), 
+    follow: (userId) => dispatch(followAction(userId)),
     unfollow: (userId) => dispatch(unfollowAction(userId)),
     setUsers: (users) => dispatch(setUsers(users)),
+    setCurrentPage: (currentPage) => dispatch(setCurrentPage(currentPage)),
+    setTotalUsersCount: (totalCount) => dispatch(setTotalUsersCount(totalCount)),
   };
 };
 
 const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users);
 
-export default UsersContainer
+export default UsersContainer;
