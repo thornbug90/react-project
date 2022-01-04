f4b609ce0261cb6c01460a0b2102c9662f39823f
thornbug90 <crazydev0409@gmail.com>
2022-01-04 14:06:43 +0100

    Updates

--- src/components/Dialogs/Message/Message.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import css from './../Dialogs.module.css';
-
-const Message = (props) => {
-  const { message } = props;
-  return <div className={css.message}>{message}</div>;
-};
-
-export default Message;
--- src/components/common/Preloader/Preloader.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import preloader from '../../../assets/images/preloader.svg';
-
-const Preloader = (props) => {
-  return <img src={preloader} alt='' />;
-};
-
-export default Preloader;
