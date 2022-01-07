faa455941ad5635c9c58d0ebd8a0ba33a1bcbb85
thornbug90 <crazydev0409@gmail.com>
2022-01-07 10:52:32 +0100

    96. upload user photo

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,13 +3,15 @@ import React from 'react';
 import MyPostsContainer from './MyPosts/MyPostsContainer';
 import ProfileInfo from './ProfileInfo/ProfileInfo';
 
-const Profile = ({ profile, status, updateStatus }) => {
+const Profile = ({ profile, status, updateStatus, isOwner, savePhoto }) => {
   return (
     <div>
       <ProfileInfo
+        isOwner={isOwner}
         profile={profile}
         status={status}
         updateStatus={updateStatus}
+        savePhoto={savePhoto}
       />
       <MyPostsContainer />
     </div>
