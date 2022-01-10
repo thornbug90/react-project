f378932b63f17bcc8691ad1a03a1334f304cef68
thornbug90 <crazydev0409@gmail.com>
2022-01-10 12:25:15 +0100

    98. Captcha

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -4,14 +4,14 @@ import { Navigate } from 'react-router-dom';
 
 import LoginForm from './LoginForm';
 
-const Login = ({ isAuth, login }) => {
+const Login = ({ isAuth, login, captcha }) => {
   if (isAuth) {
     return <Navigate to={'/profile'} />;
   }
   return (
     <div className={css.formWrapper}>
       <h1>Login</h1>
-      <LoginForm login={login} />
+      <LoginForm login={login} captcha={captcha} />
     </div>
   );
 };
